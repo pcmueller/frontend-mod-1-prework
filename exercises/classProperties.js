8f65e89ef4902138f7364939ae4b53cae1528fe1
Peter <>
2021-01-14 22:45:53 -0700

    Add completed classProperties.js

---
+++ exercises/classProperties.js
@@ -0,0 +1,161 @@
+/*
+In the exercise below, write your own code where indicated
+to achieve the desired result.
+
+Make sure to run the file with node in your command line.
+*/
+
+//-------------------
+// Part 1: Syntax and Naming Conventions
+//-------------------
+
+// For each prompt:
+  // Write a class skeleton
+  // Create 2 object instances and store in variables
+  // Log each variable to the console.
+  // You do not need any proerties at this time!!
+
+// // Prompt 1: Dog
+//
+// class Dog {
+// }
+//
+// var favoriteDog1 = new Dog();
+// console.log(favoriteDog1);
+//
+// var favoriteDog2 = new Dog();
+// console.log(favoriteDog2);
+//
+//
+// // Prompt 2: Snack
+//
+// class Snack {
+// }
+//
+// var coldPizza = new Snack();
+// console.log(coldPizza);
+//
+// var snickersBar = new Snack();
+// console.log(snickersBar);
+//
+// // Prompt 3: Shirt
+//
+// class Shirt {
+// }
+//
+// var buttonUp = new Shirt();
+// console.log(buttonUp);
+//
+// var tankTop = new Shirt();
+// console.log(tankTop);
+
+//-------------------
+// Part 2: Properties
+//-------------------
+
+// For each prompt:
+  // Write a class skeleton
+  // Add a constructor method
+  // Assign 3 properties (names and values of your choice) to each class
+  // Create an object instance and store in variable
+  // Log that variable to the console.
+
+// // Prompt 1: Dog
+
+// class Dog {
+//   constructor(name, age, breed) {
+//   }
+// }
+//
+// var favoriteDog1 = new Dog();
+// console.log(favoriteDog1);
+//
+// var favoriteDog2 = new Dog();
+// console.log(favoriteDog2);
+//
+// // Prompt 2: Snack
+//
+// class Snack {
+//   constructor(producer, foodType, datePurchased) {
+//   }
+// }
+//
+// var coldPizza = new Snack();
+// console.log(coldPizza);
+//
+// var snickersBar = new Snack();
+// console.log(snickersBar);
+//
+// // Prompt 3: Shirt
+//
+// class Shirt {
+//   constructor(brand, color, size) {
+//   }
+// }
+//
+// var buttonUp = new Shirt();
+// console.log(buttonUp);
+//
+// var tankTop = new Shirt();
+// console.log(tankTop);
+
+
+//-------------------
+// Part 3: Dynamic Properties
+//-------------------
+
+// For each prompt:
+  // Write a class skeleton
+  // Add a constructor method
+  // Declare 3 parameters in the consutructor
+  // Assign 3 properties (names and values of your choice), using the parameters
+  // Create 2 object instances and store in variables (passing in different arguments)
+  // Log those variables to the console.
+
+  // // Prompt 1: Dog
+  //
+  // class Dog {
+  //   constructor(name, age, breed) {
+  //     this.name = name;
+  //     this.age = age;
+  //     this.breed = breed;
+  //   }
+  // }
+  //
+  // var favoriteDog1 = new Dog("Scout", 4, "beagle");
+  // console.log(favoriteDog1);
+  //
+  // var favoriteDog2 = new Dog("Steve", 10, "pitbull");
+  // console.log(favoriteDog2);
+  //
+  // // Prompt 2: Snack
+  //
+  // class Snack {
+  //   constructor(producer, foodType, datePurchased) {
+  //     this.producer = producer;
+  //     this.type = foodType;
+  //     this.date = datePurchased;
+  //   }
+  // }
+  //
+  // var coldPizza = new Snack("Dominos", "pizza", "01/12/21");
+  // console.log(coldPizza);
+  //
+  // var snickersBar = new Snack("Mars", "candy", "12/13/13");
+  // console.log(snickersBar);
+  //
+  // // Prompt 3: Shirt
+  //
+  // class Shirt {
+  //   constructor(brand, color, size) {
+  //     this.brand = brand;
+  //     this.color = color;
+  //     this.size = size;
+  //   }
+  // }
+  //
+  // var buttonUp = new Shirt("Izod", "navy blue", "medium");
+  // console.log(buttonUp);
+  //
+  // var tankTop = new Shirt("H&M", "black", "large");
+  // console.log(tankTop);
